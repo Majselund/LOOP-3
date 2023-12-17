7130e3a57f9d644cdba5ad4dcc88a6c842e51150
majselund <majsebergan@gmail.com>
2023-12-17 13:36:03 +0100

    Added image 2 to edit pages on admin

--- admin/edit_about.php
+++ admin/edit_about.php
@@ -16,15 +16,15 @@ if ($getPage->num_rows > 0) {
         $text1 = $page['text1'];
         $text2 = $page['text2'];
         $imageName = $page["image"];
+        $image2Name = $page["image2"];  // Add this line
         $imageURL = '/../images/' . $page["image"];
+        $image2URL = '/../images/' . $page["image2"];  // Add this line
     }
 }
-
 
 $statusMsg = '';
 
 if (isset($_POST['submit'])) {
-
     $page = 'about';
     $title = $_POST['title'];
     $text1 = $_POST['page_editor1'];
@@ -32,6 +32,7 @@ if (isset($_POST['submit'])) {
 
     $targetDir = '/var/www/innovationsdage.dk/public_html/images/';
 
+    // Handle Image 1
     if (!empty($_FILES["image"]["name"])) {
         $imageName = basename($_FILES["image"]["name"]);
         $targetImagePath = $targetDir . $imageName;
@@ -40,6 +41,7 @@ if (isset($_POST['submit'])) {
         $allowTypes = array('jpg', 'png', 'jpeg', 'gif', 'avif', 'webp');
         if (in_array($imageType, $allowTypes)) {
             if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetImagePath)) {
+                // Update database for Image 1
                 $sql = "UPDATE pages SET title=?, text1=?, text2=?, image=? WHERE page=?";
                 $stmt = $mysqli->stmt_init();
 
@@ -60,7 +62,6 @@ if (isset($_POST['submit'])) {
                     $statusMsg = "The editor content has been inserted successfully.";
                 } else {
                     $statusMsg = "Some problem occurred, please try again.";
-                    // die($mysqli->error . " " . $mysqli->errno);
                 }
             } else {
                 $statusMsg = "Sorry, there was an error uploading your file.";
@@ -69,6 +70,7 @@ if (isset($_POST['submit'])) {
             $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.';
         }
     } else {
+        // Update database without changing the image
         $sql = "UPDATE pages SET title=?, text1=?, text2=? WHERE page=?";
         $stmt = $mysqli->stmt_init();
 
@@ -88,7 +90,42 @@ if (isset($_POST['submit'])) {
             $statusMsg = "The editor content has been inserted successfully.";
         } else {
             $statusMsg = "Some problem occurred, please try again.";
-            // die($mysqli->error . " " . $mysqli->errno);
+        }
+    }
+
+    // Handle Image 2
+    if (!empty($_FILES["image2"]["name"])) {
+        $image2Name = basename($_FILES["image2"]["name"]);
+        $targetImage2Path = $targetDir . $image2Name;
+        $image2Type = pathinfo($targetImage2Path, PATHINFO_EXTENSION);
+
+        $allowTypes = array('jpg', 'png', 'jpeg', 'gif', 'avif', 'webp');
+        if (in_array($image2Type, $allowTypes)) {
+            if (move_uploaded_file($_FILES["image2"]["tmp_name"], $targetImage2Path)) {
+                // Update database for Image 2
+                $sql = "UPDATE pages SET image2=? WHERE page=?";
+                $stmt = $mysqli->stmt_init();
+
+                if (!$stmt->prepare($sql)) {
+                    die("SQL error: " . $mysqli->error);
+                }
+
+                $stmt->bind_param(
+                    "ss",
+                    $image2Name,
+                    $page
+                );
+
+                if ($stmt->execute()) {
+                    $statusMsg = "The second image has been inserted successfully.";
+                } else {
+                    $statusMsg = "Some problem occurred, please try again.";
+                }
+            } else {
+                $statusMsg = "Sorry, there was an error uploading your second file.";
+            }
+        } else {
+            $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.';
         }
     }
 }
@@ -122,14 +159,27 @@ if (isset($_POST['submit'])) {
                         <label for="about">Skift overskrift</label><br>
                         <input type="text" id="title" name="title" value="<?php echo $title ?>">
                         <textarea name="page_editor1" id="page_editor"><?php echo $text1 ?></textarea>
+
+                        <label for="image">Image</label>
+                        <input type="file" name="image" id="image">
+
                         <?php if ($imageName) { ?>
                             <img src="<?php echo $imageURL; ?>" alt="<?php echo $imageName; ?>" class="block prose" height="300px" />
                         <?php } else { ?>
                             <p>Intet billede sat</p>
                         <?php } ?>
-                        <label for="image">Image</label>
-                        <input type="file" name="image" id="image">
+
                         <textarea name="page_editor2" id="page_editor"><?php echo $text2 ?></textarea>
+
+                        <label for="image2">Image 2</label>
+                        <input type="file" name="image2" id="image2">
+
+                        <?php if ($image2Name) { ?>
+                            <img src="<?php echo $image2URL; ?>" alt="<?php echo $image2Name; ?>" class="block prose" height="300px" />
+                        <?php } else { ?>
+                            <p>Intet billede sat</p>
+                        <?php } ?>
+
                         <input type="submit" name="submit" value="GEM">
                     </form>
                 </div>
